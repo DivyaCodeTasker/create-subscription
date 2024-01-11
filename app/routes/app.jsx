b349b0f158fc3a8b34c1ef02a9b0bd09ccc3a30d
DivyaCodeTasker <divya@codetasker.com>
2024-01-11 16:29:40 +0530

    First Commit

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -5,86 +5,55 @@ import { boundary } from "@shopify/shopify-app-remix/server";
 import { AppProvider } from "@shopify/shopify-app-remix/react";
 import {  MONTHLY_PLAN,authenticate } from "../shopify.server";
 
-
-export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
-const client = new shopify.clients.Graphql({session});
-const data = await client.query({
-  data: {
-    "query": `mutation AppPurchaseOneTimeCreate($name: String!, $price: MoneyInput!, $returnUrl: URL!) {
-      appPurchaseOneTimeCreate(name: $name, returnUrl: $returnUrl, price: $price) {
-        userErrors {
-          field
-          message
-        }
-        appPurchaseOneTime {
-          createdAt
-          id
-        }
-        confirmationUrl
+export const loader = async ({ request }) => {
+  const { admin, billing  } = await authenticate.admin(request);
+  const result = await admin.graphql(`
+  #graphql
+  query Shop {
+  app {
+    installation {
+      launchUrl
+      activeSubscriptions {
+        id
+        name
+        createdAt
+        returnUrl
+        status
+        currentPeriodEnd
+        trialDays
       }
-    }`,
+    }
+  }
+}`,
+    { variables: {} }
+  );
 
-    "variables": {
-      "name": "1000 imported orders.",
-      "returnUrl": "http://super-duper.shopifyapps.com/",
-      "price": {
-        "amount": 10.0,
-        "currencyCode": "USD"
+  const resultJson = await result.json();
+
+  const { activeSubscriptions, launchUrl  } = resultJson.data.app.installation;
+
+  console.log("Result AS: ", activeSubscriptions)
+
+    if (activeSubscriptions.length < 1) {
+     await billing.require(
+      {
+        plans: [MONTHLY_PLAN],
+        isTest: true,
+        onFailure: async () => {
+          billing.request(
+            {
+              plan: MONTHLY_PLAN,
+              returnURL: launchUrl
+            }
+          )
+        }
       }
-    },
-  },
-});
-console.log("Data.....", data);
+    )
+     console.log("Active: ", activeSubscriptions);
+   }
 
-// export const loader = async ({ request }) => {
-//   const { admin, billing  } = await authenticate.admin(request);
-//   const result = await admin.graphql(`
-//   #graphql
-//   query Shop {
-//   app {
-//     installation {
-//       launchUrl
-//       activeSubscriptions {
-//         id
-//         name
-//         createdAt
-//         returnUrl
-//         status
-//         currentPeriodEnd
-//         trialDays
-//       }
-//     }
-//   }
-// }`,
-//     { variables: {} }
-//   );
-
-//   const resultJson = await result.json();
-
-//   const { activeSubscriptions, launchUrl  } = resultJson.data.app.installation;
-
-//   console.log("Result AS: ", activeSubscriptions)
-
-//     if (activeSubscriptions.length < 1) {
-//      await billing.require(
-//       {
-//         plans: [MONTHLY_PLAN],
-//         isTest: true,
-//         onFailure: async () => {
-//           billing.request(
-//             {
-//               plan: MONTHLY_PLAN,
-//               returnURL: launchUrl
-//             }
-//           )
-//         }
-//       }
-//     )
-//      console.log("Active: ", activeSubscriptions);
-//    }
-
-//   return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
-// };
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+};
 
 export default function App() {
   const { apiKey } = useLoaderData();
